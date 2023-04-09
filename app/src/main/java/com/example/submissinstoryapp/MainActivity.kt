d0c9a8f6afde5fe8340a28afcd8bf05582c33ace
Rzldo <ezas4531@gmail.com>
2023-04-09 22:13:08 +0700

    add splash Screen

--- app/src/main/java/com/example/submissinstoryapp/MainActivity.kt
+++ app/src/main/java/com/example/submissinstoryapp/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.submissinstoryapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.submissinstoryapp.databinding.ActivityMainBinding
+import com.example.submissinstoryapp.presentation.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var _binding : ActivityMainBinding
+    companion object{
+        private const val DELAY : Long = 4000
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+        supportActionBar?.hide()
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+        }, DELAY)
     }
 }
